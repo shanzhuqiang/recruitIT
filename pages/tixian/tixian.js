c156df0b3de94afdbe5486e02b8df550729432f2
shanzhuqiang <494614349@qq.com>
2019-10-30 00:32:57 +0800

    Signed-off-by: shanzhuqiang <494614349@qq.com>

--- pages/tixian/tixian.js
+++ pages/tixian/tixian.js
@@ -21,6 +21,67 @@ Page({
       totalCoin: options.totalCoin
     })
   },
+  // 提现
+  tixianBtn () {
+    if (this.data.totalCoin < 30) {
+      wx.showModal({
+        showCancel: false,
+        title: '提示',
+        content: '余额不足',
+      })
+    } else {
+      if (this.data.money < 30) {
+        wx.showModal({
+          showCancel: false,
+          title: '提示',
+          content: '30元起提现',
+        })
+      } else {
+        wx.request({
+          url: `${app.globalData.baseUrl}/Cash/withdraw.html`,
+          data: {
+            sess_key: app.globalData.sess_key,
+            cash: this.data.money
+          },
+          method: 'POST',
+          success: (res) => {
+            wx.hideLoading()
+            if (res.data.error_code == 0) {
+              if (res.data.bizobj.result_code === 'SUCCESS') {
+                wx.showToast({
+                  title: '提现成功',
+                  mask: true,
+                  icon: 'success',
+                  success: () => {
+                    // this.getInfo(this.data.id)
+                  }
+                })
+              } else {
+                wx.showModal({
+                  showCancel: false,
+                  title: '提示',
+                  content: res.data.bizobj.err_code_des,
+                })
+              }
+              console.log(res)
+            } else {
+              wx.showModal({
+                showCancel: false,
+                title: '提示',
+                content: res.data.msg,
+              })
+            }
+          },
+          fail: (res) => {
+            wx.showToast({
+              icon: 'none',
+              title: '网络请求失败',
+            })
+          }
+        })
+      }
+    }
+  },
   // 输入金融
   moneyChange (e) {
     let money = e.detail.value
